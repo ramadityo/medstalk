c8c167e918ef199f159f7850fc0eb2aba523df80
_userNull <ramzprivatexd@gmail.com>
2024-06-09 18:23:16 +0700

    change API and fix error undefined

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,12 @@ const nextConfig = {
             },
             {
                 protocol: "https",
+                hostname: "p19-sign.tiktokcdn-us.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
                 hostname: "p16-sign.tiktokcdn-us.com",
                 port: "",
                 pathname: "/**",
--- src/api/TiktokGetUserInfo.js
+++ src/api/TiktokGetUserInfo.js
@@ -4,14 +4,14 @@ import axios from "axios";
 async function TiktokGetUserInfo(username) {
     const options = {
         method: "GET",
-        url: `${process.env.TIKTOK_USER_INFO_API_2}`,
+        url: `${process.env.NEXT_PUBLIC_TIKTOK_USER_INFO_API_2}`,
         params: {
             // username: username,
             user_name: username,
         },
         headers: {
-            // "x-rapidapi-key": `${process.env.NEXT_PUBLIC_RAPID_API_KEY}`,
-            "x-rapidapi-key": `${process.env.RAPID_API_KEY}`,
+            "x-rapidapi-key": `${process.env.NEXT_PUBLIC_RAPID_API_KEY}`,
+            // "x-rapidapi-key": `${process.env.RAPID_API_KEY}`,
             "x-rapidapi-host": "tiktok-scraper2.p.rapidapi.com",
         },
     };
