c8318d07a1d2c851d14c5c5224f2f98b3dc1b188
_userNull <ramzprivatexd@gmail.com>
2024-09-16 15:31:41 +0700

    add responsive, fix bugs on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,20 @@ import Link from "next/link";
 
 function Navbar() {
     return (
-        <nav className="container relative z-10 flex justify-between mx-auto text-xl text-white lg:px-10 py-7 flex-1/2">
-            <h1 className="text-3xl font-hndExtBold">Medstalk</h1>
+        <nav className="container relative z-10 flex justify-between items-center mx-auto text-xl text-white wide:px-0 desktop:px-10 mobile:px-4 py-7 flex-1/2">
+            <h1 className="text-3xl mobile:text-xl font-hndExtBold">Medstalk</h1>
 
-            <ul className="flex gap-4">
+            <ul className="flex gap-4 items-center">
                 <li className="relative font-hndMedium group">
-                    <Link href="#">About</Link>
+                    <Link href="#" className="mobile:text-lg">
+                        About
+                    </Link>
                     <span className="transition-all absolute bottom-0 left-0 right-0 h-[2px] bg-white origin-center scale-x-0 group-hover:scale-x-100"></span>
                 </li>
                 <li className="relative font-hndMedium group">
-                    <Link href="https://github.com/ramadityo/medstalk">Github</Link>
+                    <Link href="https://github.com/ramadityo/medstalk" className="mobile:text-lg">
+                        Github
+                    </Link>
                     <span className="transition-all absolute bottom-0 left-0 right-0 h-[2px] bg-white origin-center scale-x-0 group-hover:scale-x-100"></span>
                 </li>
             </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        screens: {
+            wide: { min: "1441px" },
+            desktop: { max: "1440px" },
+            tablet: { max: "884px" },
+            mobile: { max: "500px" },
+        },
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
 
-    fontFamily: {
-      'hndReg': ["hnd-Reg", "sans-serif"],
-      'hndRegIta': ["hnd-RegIta", "sans-serif"],
-      'hndMedium': ["hnd-Medium", "sans-serif"],
-      'hndMedIta': ["hnd-MedIta", "sans-serif"],
-      'hndBold': ["hnd-Bold", "sans-serif"],
-      'hndBoldIta': ["hnd-BoldIta", "sans-serif"],
-      'hndExtBold': ["hnd-ExtBold", "sans-serif"],
+        fontFamily: {
+            hndReg: ["hnd-Reg", "sans-serif"],
+            hndRegIta: ["hnd-RegIta", "sans-serif"],
+            hndMedium: ["hnd-Medium", "sans-serif"],
+            hndMedIta: ["hnd-MedIta", "sans-serif"],
+            hndBold: ["hnd-Bold", "sans-serif"],
+            hndBoldIta: ["hnd-BoldIta", "sans-serif"],
+            hndExtBold: ["hnd-ExtBold", "sans-serif"],
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
